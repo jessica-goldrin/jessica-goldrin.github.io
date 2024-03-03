7145d23445e387d26bf00b07849768f979bb8994
jgold <jgoldrin@uwaterloo.ca>
2024-03-03 16:22:52 -0500

    Clicking functionality for name

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,7 +18,9 @@ const Navbar = () => {
     return (
         <div className="navbar">
             <div className="wrapper">
-                <motion.span initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>Jessica Goldrin</motion.span>
+                <ScrollLink to="Homepage" spy={true} smooth={true} offset={-70} duration={500}>
+                    <motion.span initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>Jessica Goldrin</motion.span>
+                </ScrollLink>
                 <div className="navItems">
                     <motion.button className="navItem" onClick={openResume} initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }}>
                         RESUME
